b2ccc9e2d2b95965306a6eead7861d8e90aa0e9d
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-06-23 13:57:38 -0700

    ta20

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -4,11 +4,11 @@
  * main - prints the alphabet in lowercase,
  * followed by a new line
  * Return: Always 0
-*/ 
+*/
 int main(void)
 {
 	char ch;
-	
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
