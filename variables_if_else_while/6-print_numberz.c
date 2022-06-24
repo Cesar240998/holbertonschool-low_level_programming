e726cb1eeaf07cabe90157c05233647f9e2e4f3d
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-06-23 21:50:33 -0700

    tas6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 10
+ * followed by a new line, except q and e
+ * Return: Aways 0
+ */
+int main(void)
+{	int num;
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
