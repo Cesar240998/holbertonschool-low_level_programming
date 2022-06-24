766f3f1e2c9c207e33f11d22912c1c2b52b84e96
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-06-23 22:10:29 -0700

    tas7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 10
+ * followed by a new line, except q and e
+ * Return: Aways 0
+ */
+int main(void)
+{	char num;
+	for (num = 'z'; num >='a' ; num--)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
