b3ef80ca94a1be978dd30a418a5f502c93ea9d6b
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-06-25 17:16:14 -0700

    t3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Return: the result of the operation
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for(n = 0; n < 10; n++)
+	{	
+		_putchar(c);
+	}_
+	_putchar("\n");
+
+	return (0);
+}
