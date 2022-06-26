4bfc3cc96fed1bace424cc3c13aa115bb210fca3
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-06-26 00:40:28 -0700

    t3t3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * mul - multiplies two integers
- * @a: int to be multiplied to b
- * @b: int to be multiplied to a
- * Return: the result of the operation
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
  */
 void print_numbers(void)
 {
 	int n;
 
-	for(n = 0; n < 10; n++)
+	for (n = 0; n < 10; n++)
 	{	
 		_putchar(n + '0');
 	}
