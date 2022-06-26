6d4d37afceb831413e38211a42a2bec9159a96f9
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-06-25 17:06:20 -0700

    tt2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * mul - checks for uppercase character
- * @c: the character to be checked
- * Return: 1 if c is uppercase, 0 otherwise
+ * mul - multiplies two integers
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Return: the result of the operation
  */
 int mul(int a, int b)
 {
 	int c;
+
 	c = a * b;
+
 	return (c);
 }
