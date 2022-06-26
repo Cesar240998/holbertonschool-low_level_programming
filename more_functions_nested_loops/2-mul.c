6cb25feee783bcfea28a5f2d54aa2ad51939f63b
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-06-25 17:03:17 -0700

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int mul(int a, int b)
+{
+	int c;
+	c = a * b;
+	return (c);
+}
