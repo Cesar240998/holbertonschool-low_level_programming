1a95f4d2cc6e0c7485d61b99c871a72b5ba8e2c8
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-06-26 22:34:21 -0700

    t77

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,15 +10,18 @@
 
 void print_diagonal(int n)
 {
-	int count = 0;
+	int count1, count2;
 
 	if (n > 0)
 	{
-		while (count < n)
+		for (count1 = 0; count1 < n; count1++)
 		{
+			for (count2 = 0; count2 < count1; count2++)
+			{
+				_putchar(' ');
+			}
 			_putchar('\\');
 			_putchar('\n');
-			count++;
 		}
 	}
 	_putchar('\n');
