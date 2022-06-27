fa970886ff8446be8e08bc2b6685bbe9a916448d
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-06-27 10:14:47 -0700

    t10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle.
+ *
+ * @size: ampount of "#"
+ *
+ * Return: Void
+ */
+
+void print_square(int size)
+{
+	int n, x;
+
+	if (size > 0)
+	{
+		for (x = 1 ; x <= size; x++)
+		{
+			for (n = x; n < size; n++)
+			{
+				_putchar(' ');
+			}
+			
+			for (n = 1; n <= x; n++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	} else
+	_putchar('\n');
+}
