856c6abf79ef2defeecbb669057613c81a80a892
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-06-26 23:35:39 -0700

    tt8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square.
+ *
+ * @size: ampount of "#"
+ *
+ * Return: Void
+ */
+
+void print_square(int size);
+{
+	int n, x;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (n = 1; n < size; n++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			_putchar('#');
+		}
+	} else
+	_putchar('\n');
+}
