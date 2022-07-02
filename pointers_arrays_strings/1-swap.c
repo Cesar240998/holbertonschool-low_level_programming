5b4ba602abd2d0c2bdc4fdc05fd3edc2e50549fa
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-07-02 11:12:01 -0700

    tt1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - updates the value of the variable
+ * @*a: pointer to the variable to update
+ * @*b: another one
+ * */
+
+void swap_int(int *a, int *b)
+{
+	int n = *a;
+
+	*a = *b;
+	*b = n;
+}
