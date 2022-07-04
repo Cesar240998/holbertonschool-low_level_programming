c6f4ae6ad2cf2baa198c968638cd54b29964ec3c
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-07-03 23:54:52 -0700

    t5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - returns length of a string
+ * @s: pointer to the variable to up
+ * Return: void
+ **/
+void rev_string(char *s)
+{
+	int size = 0;
+	int n;
+
+	while (*s != '\0')
+	{
+		_putchar(s);
+		size++;
+		s++;
+	}
+	s--;
+	for (n = 0; n < size; n++)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
