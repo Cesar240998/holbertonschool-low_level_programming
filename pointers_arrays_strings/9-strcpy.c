bdcfa8b9fd91b6040390b81e15e1c186fe14dac4
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-07-07 11:40:27 -0700

    t99

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcpy - prints half of a string
+ * followed by a new line
+ * @dest: string to be printed
+ * @src: number of elements
+ * Return: the pointer to dest
+ */
+void *_strcpy(char *dest, char *src)
+{
+	int len, n;
+
+	len = 0;
+	
+	while (src[len] != '\0')
+	{	
+		len++;
+	}
+
+	for (n = 0; n < len; n++)
+	{
+		dest[n] = src[n];
+	}
+	dest[n] = '\0';
+
+	return(dest);
+}
