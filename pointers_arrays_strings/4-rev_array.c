93c5f527dd0da37bdbcc7036552dc7bef0a18a97
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-07-08 09:37:18 -0700

    t44

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_array - compares two strings
+ * @a: first string
+ * @n: second string
+ */
+void reverse_array(int *a, int *b)
+{
+	int len, len1, temp;
+
+	len1 = n - 1;
+	for (len = 0; len < n / 2; len++)
+	{
+		temp = a[len];
+		a[len] = a[len1];
+		a[len1--] = temp;
+	}
+}
