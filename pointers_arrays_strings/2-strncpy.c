47fef50632876f22a2ba52365206d8898745f917
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-07-07 23:53:46 -0700

    t2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strncpy - prints half of a string
+ * followed by a new line
+ * @dest: string to be printed
+ * @src: number of elements
+ * @n: numb of bytes
+ * Return: the pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len;
+
+	len = 0;
+	while (src[len] != '\0' && len < n)
+	{
+		dest[len] = src[len];
+		len++;
+	}
+
+	return (dest);
+}
