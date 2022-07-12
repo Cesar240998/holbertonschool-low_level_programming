cf0390141748e72a5073d92014649c8d69ee3354
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-07-12 11:34:22 -0700

    t1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memcpy - fills memory with a constant byte
+ * @dest: memory area to be filled
+ * @char: char to copy
+ * @n: number of times to copy b
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *dest, char *src, unsigned int n)
+{
+	unsigned int y;
+
+	for (y = 0; y < n; y++)
+	{
+		dest[y] = src[y];
+	}
+
+	return (dest);
+}
