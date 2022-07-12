b8069305e9ed07958275373b5105a194f289a74b
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-07-12 11:50:52 -0700

    t22

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strchr - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @c: char to copy
+ * Return: pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int y;
+
+	for (y = 0; y < n; y++)
+	{
+		if (s[y] = c)
+		tmp[] = s[y+1];
+	}
+
+	return (temp);
+}
