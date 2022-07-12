b2cb1fbf8f6cbcbf1594b9d3234fbc928d33d5d7
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-07-12 11:37:11 -0700

    1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -3,7 +3,7 @@
 /**
  * *_memcpy - fills memory with a constant byte
  * @dest: memory area to be filled
- * @char: char to copy
+ * @src: char to copy
  * @n: number of times to copy b
  * Return: pointer to the memory area s
  */
