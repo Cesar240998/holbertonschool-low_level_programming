8de404f50e8270b23573b974b6a7baf33883c1a2
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-07-14 22:06:14 -0700

    t4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strpbrk - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @accept: char to copy
+ * Return: pointer to the memory area s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+		int i, j;
+
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			for (j =0; accept[j] != '\0'; j++)
+				if (*s == accept[j])
+					return (s);
+		}
+		return (NULL);
+}
