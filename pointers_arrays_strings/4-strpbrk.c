4cabccdb3061fad7536c6d052c267fe8f6072adc
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-07-14 22:17:31 -0700

    44

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -14,8 +14,11 @@ char *_strpbrk(char *s, char *accept)
 		for (i = 0; *s != '\0'; i++)
 		{
 			for (j = 0; accept[j] != '\0'; j++)
+			{
 				if (*s == accept[j])
 					return (s);
+			}
+		s++;
 		}
 		return (NULL);
 }
