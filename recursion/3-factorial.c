1e6ce02eb7a268694c0e087198da7794d4cb8a8d
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-07-15 21:35:34 -0700

    t33

---
+++ recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * factorial - prints a string, followed by a new line
+ * @n: string to print
+ *
+ * Return: factorial
+ */
+int factorial(char n)
+{
+	if(i <= 1)
+		return 1;
+   	return i * factorial(i - 1);
+}
