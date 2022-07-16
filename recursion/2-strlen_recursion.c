c11a729870446280e2bfe6633f07c9c6e60ead7c
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-07-15 21:10:34 -0700

    t2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints a string, followed by a new line
+ * @s: string to print
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
