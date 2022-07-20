4c0365d0713ac2206bee014166cdd9466399642b
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-07-20 09:41:29 -0700

    t22

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _calloc - Allocates memory for an array, using malloc.*
+ * @nmemb: number of elements
+ * @size: size of an element
+ *
+ * Return: hola
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, fsize = nmemb * size;
+	char *array = NULL;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	array = malloc(fsize);
+
+	if (!array)
+	{
+		free(array);
+		return (NULL);
+	}
+
+	for (i = 0; i < fsize; i++)
+		array[i] = 0;
+
+	return (array);
+}
