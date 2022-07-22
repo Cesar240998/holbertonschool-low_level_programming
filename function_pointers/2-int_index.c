4d205b5890d12c1bbfc15657fb0020e830549a3f
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-07-21 21:46:05 -0700

    t02

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -15,15 +15,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0 || !array || !cmp)
 		return (-1);
-	else
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			res = cmp(array[i]);
-			
-			if (res != 0)
-				return (i);
-		}
-	return (-1);
+		res = cmp(array[i]);
+		
+		if (res != 0)
+			return (i);
 	}
+	return (-1);
 }
