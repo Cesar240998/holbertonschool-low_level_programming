61e33f749c45059338c9cce641294536a62d8f84
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-07-22 14:37:03 -0700

    t2

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - Print all the numbers passed like arguments
+ * @separator: String to separate numbers
+ * @n: Number of arguments
+ *
+ * Return: Void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list string;
+	unsigned int i;
+
+	va_start(string, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(string, char));
+		if (i < (n - 1) && separator)
+			printf("%s", separator);
+	}
+
+	va_end(string);
+	putchar(10);
+}
