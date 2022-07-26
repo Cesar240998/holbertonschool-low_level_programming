9fd1ede8bc55c9789ac155f7777ab5088d5157ff
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-07-26 16:26:53 -0700

    t4

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * clear_bit - Entry Point
+ * @n: input
+ * @index: index
+ * Return: 0
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bits;
+
+	if (index > 63)
+		return (-1);
+
+	bits = 1 << index;
+
+	*n = *n &(~bits);
+
+	return (1);
+}
