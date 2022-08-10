4eba5ae0f566d1a00a9d15b792edf83f1d855635
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-08-09 21:07:30 -0700

    t7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,52 +1,42 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts node at index
- * @h: head of node
- * @idx: index to insert node
- * @n: data for new node
- * Return: list with inserted node
+ * insert_dnodeint_at_index - Inserts a new node at a given position.
+ * @h: Double pointer to struct.
+ * @idx: Index position to insert node.
+ * @n: Value of new node.
+ *
+ * Return: The address of the new node, or NULL if it failed.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int count = 1;
-	dlistint_t *temp = NULL, *new = NULL;
+	dlistint_t *node = NULL;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL || h == NULL)
+	if (h == NULL)
 		return (NULL);
-	new->n = n;
-	temp = *h;
+
 	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	if (idx > 1 && *h != NULL)
+		return (insert_dnodeint_at_index(&((*h)->next), idx - 1, n));
+
+	if (idx > 1 || *h == NULL)
+		return (NULL);
+
+	if ((*h)->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
 	{
-		*h = new;
-		new->next = temp;
-		new->prev = NULL;
-		temp->prev = new;
-		return (new);
-	}
-	while (temp->next != NULL)
-	{
-		if (count == idx) 
-		{
-			new->prev = temp; 
-			new->next = temp->next; 
-			temp->next = new; 
-			new->next->prev = new; 
-		}
-		temp = temp->next;
-		count++;
-	}
-	if (count == idx) 
-	{
-		new->prev = temp; 
-		new->next = NULL; 
-		temp->next = new; 
-	}
-	if (count < idx)
-	{
-		free(new);
+		dprintf(2, "Error: Can't malloc\n");
 		return (NULL);
 	}
-	return (new);
+	node->n = n;
+	node->next = (*h)->next;
+	node->prev = (*h);
+	(*h)->next->prev = node;
+	(*h)->next = node;
+	return (node);
 }
